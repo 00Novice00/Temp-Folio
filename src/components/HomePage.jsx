7ac53d2ac55bda51813e5346f04ff16c31b3ced1
00Novice00 <roh4n.b@gmail.com>
2023-09-12 17:02:51 +0530

    simplified namings

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -34,7 +34,7 @@ const HomePage = () => {
 
 
     return (
-        <div className='hp-border'>
+        <div className='homepage-hp'>
             <nav className='hp-navbar'>
                 <div className='hp-navcontent container'>
                     <a className='hp-logo' href="#home"  onClick={() => handleHome(false)}>TF</a>
@@ -62,17 +62,17 @@ const HomePage = () => {
             </div>}
 
             {!project && <div>
-                <section id='home' className='hp-homesection'>
-                    <div className='hp-myinfo'>
-                        <h1 className='hp-myname'>HEY, I'M "YOUR NAME"</h1>
-                        <p className='hp-shortnote'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, nobis eius obcaecati nulla sequi modi?</p>
-                        <div className='hp-homesocials'>
-                            <div className='hp-socialresponsive '>
-                                <div className='hp-footersocials'>
-                                    <a href="#" target='_blank' rel="noopener noreferrer " title="linkedin"><BsLinkedin size={25} className='hp-footericons' /></a>
-                                    <a href="#" target='_blank' rel="noopener noreferrer " title="twitter"><FaTwitter size={25} className='hp-footericons' /></a>
-                                    <a href="#" target='_blank' rel="noopener noreferrer " title="mail"><FiMail size={25} className='hp-footericons' /></a>
-                                    <a href="#" target='_blank' rel="noopener noreferrer " title="github"><BsGithub size={25} className='hp-footericons' /></a>
+                <section id='home' className='homesection-hs'>
+                    <div className='myinfo-hs'>
+                        <h1 className='myname-hs'>HEY, I'M "YOUR NAME"</h1>
+                        <p className='shortnote-hs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, nobis eius obcaecati nulla sequi modi?</p>
+                        <div className='homesocials-hs'>
+                            <div className='socialresponsive-hs '>
+                                <div className='mobilesocials-hs'>
+                                    <a href="#" target='_blank' rel="noopener noreferrer " title="linkedin"><BsLinkedin size={25} className='mobileicons' /></a>
+                                    <a href="#" target='_blank' rel="noopener noreferrer " title="twitter"><FaTwitter size={25} className='mobileicons' /></a>
+                                    <a href="#" target='_blank' rel="noopener noreferrer " title="mail"><FiMail size={25} className='mobileicons' /></a>
+                                    <a href="#" target='_blank' rel="noopener noreferrer " title="github"><BsGithub size={25} className='mobileicons' /></a>
                                 </div>
                             </div>
                         </div>
@@ -89,24 +89,24 @@ const HomePage = () => {
                 {/* ////////////////////////////////ABOUTSECTION////////////////////////////// */}
 
 
-                <section className='hp-aboutme' id='about'>
+                <section className='aboutsection-as' id='about'>
                     <div className='container'>
-                        <div className='hp-headline'>
+                        <div className='headline'>
                             <h1>ABOUT ME</h1>
-                            <hr className='hp-hrline' />
+                            <hr className='hrline' />
                         </div>
-                        <div className='hp-sections'>
-                            <div className='hp-divsection'>
-                                <h1 className='hp-subheading'>Get to know me!</h1>
-                                <p className='hp-info'>Hello! I'm <span>"YOUR NAME"</span>,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas fugit unde sed alias obcaecati est mollitia et quaerat voluptatum consequuntur magnam possimus iusto eius ab labore sequi, suscipit perspiciatis doloribus reprehenderit soluta dignissimos voluptates esse ea. Rem voluptates.</p>
+                        <div className='mainsection-as'>
+                            <div className='divsection-as'>
+                                <h1 className='subheading-as'>Get to know me!</h1>
+                                <p className='info-as'>Hello! I'm <span>"YOUR NAME"</span>,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas fugit unde sed alias obcaecati est mollitia et quaerat voluptatum consequuntur magnam possimus iusto eius ab labore sequi, suscipit perspiciatis doloribus reprehenderit soluta dignissimos voluptates esse ea. Rem voluptates.</p>
                                 <br />
-                                <p className='hp-info'>I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills then click below and fill out the form or you can find my socials and phone number to contact me.</p>
+                                <p className='info-as'>I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills then click below and fill out the form or you can find my socials and phone number to contact me.</p>
                                 <a className='button' href="#contactme"><span>CONTACT</span></a>
                             </div>
-                            <div className='hp-divsection'>
-                                <h1 className='hp-subheading'>My Skills</h1>
-                                <div className='hp-skills'>
-                                    {['HTML', 'CSS', 'JavaScript', 'React', 'Java', 'MySQL', 'HTML', 'CSS', 'JavaScript'].map((item, index) => <div key={index} className='hp-singleskill'>{item}</div>)}
+                            <div className='divsection-as'>
+                                <h1 className='subheading-as'>My Skills</h1>
+                                <div className='skills-as'>
+                                    {['HTML', 'CSS', 'JavaScript', 'React', 'Java', 'MySQL', 'HTML', 'CSS', 'JavaScript'].map((item, index) => <div key={index} className='singleskill-as'>{item}</div>)}
 
                                 </div>
                             </div>
@@ -116,57 +116,57 @@ const HomePage = () => {
                 </section>
 
 
-                {/* ////////////////////////////////PROJECTSECTION 01////////////////////////////// */}
+                {/* ////////////////////////////////PROJECTSECTION////////////////////////////// */}
 
-                <section className='hp-project' id='projects'>
+                <section className='projectsection-ps' id='projects'>
                     <div className='container'>
-                        <div className='hp-headline'>
+                        <div className='headline'>
                             <h1>PROJECTS</h1>
-                            <hr className='hp-hrline' />
+                            <hr className='hrline' />
                         </div>
-                        <div className='hp-projectsections'>
-                            <div className='hp-projectsubsection devicepadding'>
-                                <img className='hp-projectimg' src={Project01} alt="Project1" />
+                        <div className='mainsection-ps'>
+                            <div classNahp-projectheading2me='subsection-ps devicepadding'>
+                                <img className='projectimg-ps' src={Project01} alt="Project1" />
                             </div>
-                            <div className='hp-projectsubsection'>
-                                <h1 className='hp-projectheading'>Project01</h1>
+                            <div className='subsection-ps'>
+                                <h1 className='heading-ps'>Project01</h1>
                                 <br />
-                                <p className='hp-projectinfo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, sed sint praesentium ad voluptates porro? ipsum dolor sit amet consectetur adipisicing elit. </p>
+                                <p className='info-ps'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, sed sint praesentium ad voluptates porro? ipsum dolor sit amet consectetur adipisicing elit. </p>
                                 <a href="#home" className='button' onClick={handleProject}><span>Know More</span></a>
                             </div>
                         </div>
-                        <div className='hp-projectsections'>
-                            <div className='hp-projectsubsection devicepadding'>
-                                <img className='hp-projectimg' src={Project02} alt="Project1" />
+                        <div className='mainsection-ps'>
+                            <div className='subsection-ps devicepadding'>
+                                <img className='projectimg-ps' src={Project02} alt="Project1" />
                             </div>
-                            <div className='hp-projectsubsection'>
-                                <h1 className='hp-projectheading'>Project02</h1>
+                            <div className='subsection-ps'>
+                                <h1 className='heading-ps'>Project02</h1>
                                 <br />
-                                <p className='hp-projectinfo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa eos tenetur iure, aspernatur laborum? ipsum dolor sit amet consectetur adipisicing elit. </p>
+                                <p className='info-ps'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa eos tenetur iure, aspernatur laborum? ipsum dolor sit amet consectetur adipisicing elit. </p>
                                 <a href="#home" className='button' onClick={handleProject}><span>Know More</span></a>
                             </div>
                         </div>
-                        <h1 className='hp-projectheading2'>Project Section 02</h1>
-                        <div className='hp-projectsection2'>
-                            <div className='hp-projectsubsection2'>
-                                <img className='hp-projectsection2images' src={Project01} alt="Project1" />
-                                <div className='txtbtn'>
-                                    <h1 className='hp-projname'>PROJECT 1</h1>
-                                    <a className='projbutton' href="#">Know More</a>
+                        <h1 className='headingsection2-ps'>Project Section 02</h1>
+                        <div className='mainsection2-ps'>
+                            <div className='subsection2-ps'>
+                                <img className='section2images-ps' src={Project01} alt="Project1" />
+                                <div className='imageoverlay-ps'>
+                                    <h1 className='projectname-ps'>PROJECT 1</h1>
+                                    <a className='projectbutton-ps' href="#">Know More</a>
                                 </div>
                             </div>
-                            <div className='hp-projectsubsection2'>
-                                <img className='hp-projectsection2images' src={Project02} alt="Project1" />
-                                <div className='txtbtn'>
-                                    <h1 className='hp-projname'>PROJECT 1</h1>
-                                    <a className='projbutton' href="#">Know More</a>
+                            <div className='subsection2-ps'>
+                                <img className='section2images-ps' src={Project02} alt="Project1" />
+                                <div className='imageoverlay-ps'>
+                                    <h1 className='projectname-ps'>PROJECT 1</h1>
+                                    <a className='projectbutton-ps' href="#">Know More</a>
                                 </div>
                             </div>
-                            <div className='hp-projectsubsection2'>
-                                <img className='hp-projectsection2images' src={Project03} alt="Project1" />
-                                <div className='txtbtn'>
-                                    <h1 className='hp-projname'>PROJECT 1</h1>
-                                    <a className='projbutton' href="#">Know More</a>
+                            <div className='subsection2-ps'>
+                                <img className='section2images-ps' src={Project03} alt="Project1" />
+                                <div className='imageoverlay-ps'>
+                                    <h1 className='projectname-ps'>PROJECT 1</h1>
+                                    <a className='projectbutton-ps' href="#">Know More</a>
                                 </div>
                             </div>
                         </div>
@@ -176,14 +176,14 @@ const HomePage = () => {
                 {/* ////////////////////////////////CONTACTSECTION////////////////////////////// */}
 
 
-                <section className='hp-contact backgroungimage' id='contact'>
+                <section className='contactsection-cs backgroungimage' id='contact'>
                     <div className='container'>
-                        <div className='hp-headline'>
+                        <div className='headline'>
                             <h1>CONTACT</h1>
-                            <hr className='hp-hrline' />
+                            <hr className='hrline' />
                         </div>
-                        <div className='hp-contentsection hp-devicepadding'>
-                            <p className='hp-contactsub'>Feel free to Contact me by submitting the form and I will get back to you as soon as possible</p>
+                        <div className='mainsection-cs hp-devicepadding'>
+                            <p className='mainsection-cs'>Feel free to Contact me by submitting the form and I will get back to you as soon as possible</p>
                             <ContactForm />
                         </div>
                     </div>
@@ -195,16 +195,16 @@ const HomePage = () => {
 
             {/* ////////////////////////////////FOOTERSECTION////////////////////////////// */}
 
-            <section className='hp-footer'>
+            <section className='footersection-fs'>
                 <div className='container'>
-                    <div className='hp-footersections'>
-                        <div className='hp-subsection '>
-                            <h1 className='hp-footerheadings'>"YOUR NAME"</h1>
-                            <p className='hp-footersubgeading'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum odit non similique.</p>
+                    <div className='mainsection-fs'>
+                        <div className='subsection-fs '>
+                            <h1 className='heading-fs'>"YOUR NAME"</h1>
+                            <p className='subheading-fs'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum odit non similique.</p>
                         </div>
-                        <div className='hp-rightsubsection'>
-                            <h1 className='hp-footerheadings'>SOCIALS</h1>
-                            <div className='hp-footersocials'>
+                        <div className='rightsubsection-fs'>
+                            <h1 className='heading-fs'>SOCIALS</h1>
+                            <div className='footersocials-fs'>
                                 <a href="#" target='_blank' rel="noopener noreferrer " title="linkedin"><BsLinkedin size={25} className='footericons' /></a>
                                 <a href="#" target='_blank' rel="noopener noreferrer " title="twitter"><FaTwitter size={25} className='footericons' /></a>
                                 <a href="#" target='_blank' rel="noopener noreferrer " title="mail"><FiMail size={25} className='footericons' /></a>
@@ -213,11 +213,11 @@ const HomePage = () => {
 
                         </div>
                     </div>
-                    <hr id='hp-footerline' />
-                    <div class="hp-main-footer">
+                    <hr id='line-fs' />
+                    <div class="copyrightsection-fs">
                         © Copyright
                         <script>document.write(new Date().getFullYear())</script>2023. Made by
-                        <a className='hp-cplink' rel="noreferrer" target="_blank" href="https://ronyb.me"> Rohan Barikar</a>
+                        <a className='copyrightlink-fs' rel="noreferrer" target="_blank" href="https://ronyb.me"> Rohan Barikar</a>
                     </div>
                 </div>
             </section>
--- src/components/Project1.jsx
+++ src/components/Project1.jsx
@@ -10,34 +10,34 @@ const Project1 = (props) => {
 
     return (
         <div>
-            <section className='homesection' id='home'>
-                <div className='myinfo container'>
-                    <h1 className='myname'>PROJECT1</h1>
+            <section className='heading-proj' id='home'>
+                <div className='projectoverview-proj container'>
+                    <h1 className='projectname-proj'>PROJECT1</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quisquam placeat neque officia qui laboriosam iure corporis consequuntur deserunt delectus.</p>
                     <a className='button button1' href="#" target='_blank'>PROJECT LINK</a>
                 </div>
             </section>
-            <section className='projectinfo' >
+            <section className='projectoverview-proj' >
                 <div className='container'>
-                    <div className='center'><img className='projectimg' src={Project01} alt="projectlogo" /></div>
+                    <div className='projectimagebox-proj'><img className='projectimage-proj' src={Project01} alt="projectlogo" /></div>
 
 
-                    <div className='projsections'>
-                        <div className='projdivsection'>
-                            <h1 className='projsubheading'>Overview</h1>
-                            <p className='projinfo1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam asperiores labore voluptatum quis esse minus quae voluptates amet qui culpa.</p>
-                            <p className='projinfo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi odit veritatis nobis consequuntur fuga eaque et voluptatum. Unde voluptatum tempora omnis voluptas explicabo exercitationem ut ab, eum blanditiis consequuntur. Cumque ea earum maiores natus quam autem ipsum, illum rem explicabo unde harum illo! Dolor eius, reprehenderit temporibus ullam animi sunt! </p>
+                    <div className='mainsection-proj'>
+                        <div className='divsection-proj'>
+                            <h1 className='overview-proj'>Overview</h1>
+                            <p className='info1-proj'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam asperiores labore voluptatum quis esse minus quae voluptates amet qui culpa.</p>
+                            <p className='info2-proj'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi odit veritatis nobis consequuntur fuga eaque et voluptatum. Unde voluptatum tempora omnis voluptas explicabo exercitationem ut ab, eum blanditiis consequuntur. Cumque ea earum maiores natus quam autem ipsum, illum rem explicabo unde harum illo! Dolor eius, reprehenderit temporibus ullam animi sunt! </p>
                         </div>
-                        <div className='projdivsection'>
-                            <h1 className='projsubheading'>Tech Used</h1>
-                            <div className='skills'>
-                                {['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Java', 'SpringBoot'].map((item, index) => <div key={index} className='singleskill'>{item}</div>)}
+                        <div className='divsection-proj'>
+                            <h1 className='overview-proj'>Tech Used</h1>
+                            <div className='skills-proj'>
+                                {['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Java', 'SpringBoot'].map((item, index) => <div key={index} className='singleskill-proj'>{item}</div>)}
 
                             </div>
 
                             <div className='buttons'>
-                                <a className='probutton' href="#" target='_blank'><span>PROJECT LINK</span></a>
-                                <a href="#home" className='gobackbutton' onClick={handleClick}><span>GO BACK</span></a>
+                                <a className='projectlinkbutton-proj' href="#" target='_blank'><span>PROJECT LINK</span></a>
+                                <a href="#home" className='gobackbutton-proj' onClick={handleClick}><span>GO BACK</span></a>
                             </div>
 
                         </div>
